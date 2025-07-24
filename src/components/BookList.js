ed7356fccaaa0bd8d9751f0bf1d2ac40aeca99bc
Djouna <djounaabsolu718@gmail.com>
2025-07-24 17:25:48 -0400

    Djouna: ajout de la barre de recherche et du filtre

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,16 +1,36 @@
-import React from 'react'; 
+import React,{useState} from 'react'; 
 import BookCard from './BookCard'
 import books from "../Books";
-
+import Search from './searchFilter'
 //Auteur : Sandlie pierre
 
  // src/components/BookList.js import React from "react"; import booksData from "../data/books";
 
 const BookList = () => { 
-  return ( 
+  const [searchTem, setSearchTerm] = useState("");
+const [category, setCategory] = useState("");
+const uniqueCategories = [...new Set(books.map((book)=>book.category))];
+
+const filteredBooks = books.filter((book) =>{
+    const matchesSearch = 
+     book.title.toLowerCase().includes(searchTem.toLowerCase())
+    ||
+      book.author.name.toLowerCase().includes(searchTem.toLowerCase());
+      const matchesCategory = category ? book.category===category : true;
+      return matchesSearch && matchesCategory;
+});
+  return (
+    
   <div className="container"> 
+  <Search 
+  searchTem={searchTem}
+  setSearchTerm={setSearchTerm}
+  category={category}
+  setCategory={setCategory}
+  categories={uniqueCategories}
+  />
   <div className="row justify-content-center"> 
-    {books.map((book) => ( 
+    {filteredBooks.map((book) => ( 
       <div className="col-md-4" key={book.id}> 
             <BookCard book={book}/>
              </div>
